87ecff62afa472fcaa03d5c4c62fdcab3119d4c3
Daniil Baunov <parezguru@gmail.com>
2016-10-26 21:10:13 +0300

    Added Heroku support

--- server/server.js
+++ server/server.js
@@ -7,12 +7,14 @@ var bodyParser = require("body-parser");
 var {mongoose} = require('./db/mongoose');
 var citationRoutes = require('./routes/citations');
 
+const port = process.env.PORT || 3000;
+
 var app = express();
 
 app.use(bodyParser.json());
 
 app.use("/citations", citationRoutes);
 
-app.listen(3000, () => {
-    console.log("Started from the bottom now we here (3000)");
+app.listen(port, () => {
+    console.log(`Started from the bottom now we here (${port})`);
 });
